5813b2180559c545f3b13444cbba364e8ba41c83
FabioPortella <fabioportella2009@gmail.com>
2024-09-04 11:55:37 -0300

    finalizando aula 3

---
+++ app.js
@@ -0,0 +1,1 @@
+console.log(filmesDeTerror90);
---
+++ dados.js
@@ -0,0 +1,77 @@
+let atletasBrasil = [
+    {
+        titulo: "Rayssa Leal",
+        descricao:
+          "Rayssa Leal, conhecida como 'Fadinha', é uma skatista profissional brasileira que, desde muito jovem, encanta o mundo com seu talento e carisma. Aos 13 anos, tornou-se a mais jovem medalhista olímpica da história do Brasil, conquistando a prata nos Jogos Olímpicos de Tóquio.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao:
+          "Rebeca Andrade é uma ginasta artística brasileira que conquistou o coração dos brasileiros e se tornou um dos maiores nomes do esporte nacional. Com sua força, determinação e talento, ela coleciona medalhas em competições nacionais e internacionais, incluindo ouro olímpico.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+    }, 
+    {
+        titulo: "Beatriz Souza",
+        descricao:
+          "Beatriz Souza é uma judoca brasileira que se destaca pela sua técnica apurada e determinação dentro do tatame. Com diversas conquistas em competições nacionais e internacionais, Beatriz é um dos principais nomes do judô feminino brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+    },
+];
+
+let filmesDeTerror90 = [
+    {
+        titulo: "O Exorcista III",
+        sinopse: "Uma sequência do clássico de terror, ambientada em um hospital psiquiátrico onde um padre exorcista enfrenta uma força demoníaca que possui um paciente.",
+        genero: "Terror, Sobrenatural"
+    },
+    {
+        titulo: "A Bruxa de Blair",
+        sinopse: "Um grupo de estudantes desaparece durante a filmagem de um documentário em uma floresta amaldiçoada, e as fitas encontradas revelam uma história de terror sobrenatural.",
+        genero: "Terror, Found Footage"
+    },
+    {
+        titulo: "Pânico",
+        sinopse: "Um assassino mascarado aterroriza uma pequena cidade, e um grupo de adolescentes se torna o alvo de seus ataques, enquanto tentam desvendar a identidade do assassino.",
+        genero: "Terror, Slasher"
+    },
+    // Adicione mais filmes aqui...
+    {
+        titulo: "Eu Sei o Que Vocês Fizeram no Verão Passado",
+        sinopse: "Um grupo de amigos é perseguido por um assassino um ano após um acidente fatal, e precisam enfrentar os seus segredos para sobreviver.",
+        genero: "Terror, Slasher"
+    }
+];
+
+let musicasAnos80 = [
+    {
+        nome: "Sweet Child O' Mine",
+        autor: "Guns N' Roses",
+        genero: "Hard Rock",
+        link: "https://www.youtube.com/watch?v=1w7OgIMMRc4"
+    },
+    {
+        nome: "Billie Jean",
+        autor: "Michael Jackson",
+        genero: "Pop, R&B",
+        link: "https://www.youtube.com/watch?v=Zi_XLOBDo_Y"
+    },
+    {
+        nome: "Livin' on a Prayer",
+        autor: "Bon Jovi",
+        genero: "Rock",
+        link: "https://www.youtube.com/watch?v=lDK9QqIzhwk"
+    },
+    {
+        nome: "Every Breath You Take",
+        autor: "The Police",
+        genero: "Rock, Pop",
+        link: "https://www.youtube.com/watch?v=OMOGaugKpzs"
+    },
+    {
+        nome: "Wake Me Up Before You Go-Go",
+        autor: "Wham!",
+        genero: "Pop",
+        link: "https://www.youtube.com/watch?v=pIgZ7gMze7A"
+    }
+];
