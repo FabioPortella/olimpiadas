69a493f238fa891661af573b9c4de42b5f9f12e6
FabioPortella <fabioportella2009@gmail.com>
2024-09-06 12:35:37 -0300

    finalizando aula 5

--- app.js
+++ app.js
@@ -1,24 +1,52 @@
 function pesquisar() {
-    // Obtém a seção onde os resultados serão exibidos
-    let section = document.getElementById("resultados-pesquisa");
+  // Obtém a seção onde os resultados serão exibidos
+  let section = document.getElementById("resultados-pesquisa");
 
-    // Inicializa uma string vazia para armazenar os resultados
-    let resultado = "";
+  // Obtém o campo-pesquisa onde será inserido o texto a ser pesquisado.
+  let campoPesquisa = document.getElementById("campo-pesquisa").value;
 
-    // Itera sobre cada dado (atleta) da lista de dados - arquivo DADOS.JS
-    for (let dado of dados) {
-        // Cria um novo elemento div para cada atleta
-        resultado += `
-            <div class="item-resultado">
-                <h2>
-                    <a href="#" target="_blank">${dado.titulo}</a>
-                </h2>
-                <p class="descricao-meta">${dado.descricao}</p>
-                <a href=${dado.link} target="_blank">Mais informações</a>
-            </div>
-        `;
+  // Se o campo pesquisa for vazio
+  if (!campoPesquisa) {
+    section.innerHTML = "<p>Digite algo no campo de pesquisa</p>";
+    return;
+  }
+
+  // Inicializa variavéis vazias
+  let resultado = "";
+  let titulo = "";
+  let descricao = "";
+  let tags = "";
+
+  // Itera sobre cada dado (atleta) da lista de dados - arquivo DADOS.JS
+  for (let dado of dados) {
+    // convertendo conteudo das variaveis para 
+    campoPesquisa = campoPesquisa.toLowerCase();
+    titulo = dado.titulo.toLowerCase();
+    descricao = dado.descricao.toLowerCase();
+    tags = dado.tags.toLowerCase();
+    // se no dado.titulo incluir o campo de pesquisa, faça o código abaixo
+    if (
+      titulo.includes(campoPesquisa) ||
+      descricao.includes(campoPesquisa) ||
+      tags.includes(campoPesquisa)
+    ) {
+      // Cria um novo elemento div para cada atleta
+      resultado += `
+                    <div class="item-resultado">
+                        <h2>
+                            <a href="#" target="_blank">${dado.titulo}</a>
+                        </h2>
+                        <p class="descricao-meta">${dado.descricao}</p>
+                        <a href=${dado.link} target="_blank">Mais informações</a>
+                    </div>
+                `;
+    }    
+  }
+
+  if (!resultado) {
+      resultado = "<p>Nada foi encontrado.</p>";
     }
 
-    // Atribui o HTML gerado à seção de resultados
-    section.innerHTML = resultado;
+  // Atribui o HTML gerado à seção de resultados
+  section.innerHTML = resultado;
 }
--- dados.js
+++ dados.js
@@ -4,17 +4,20 @@ let dados = [
         descricao:
           "Rayssa Leal, conhecida como 'Fadinha', é uma skatista profissional brasileira que, desde muito jovem, encanta o mundo com seu talento e carisma. Aos 13 anos, tornou-se a mais jovem medalhista olímpica da história do Brasil, conquistando a prata nos Jogos Olímpicos de Tóquio.",
         link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8",
     },
     {
         titulo: "Rebeca Andrade",
         descricao:
           "Rebeca Andrade é uma ginasta artística brasileira que conquistou o coração dos brasileiros e se tornou um dos maiores nomes do esporte nacional. Com sua força, determinação e talento, ela coleciona medalhas em competições nacionais e internacionais, incluindo ouro olímpico.",
         link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginastica ginástica",
     }, 
     {
         titulo: "Beatriz Souza",
         descricao:
           "Beatriz Souza é uma judoca brasileira que se destaca pela sua técnica apurada e determinação dentro do tatame. Com diversas conquistas em competições nacionais e internacionais, Beatriz é um dos principais nomes do judô feminino brasileiro.",
         link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: " judo judoca judoka",
     },
 ];
